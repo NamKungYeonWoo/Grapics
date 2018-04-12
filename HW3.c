ea74b9671ce38d0ce61fa381f5f3e846ceb9be32
손승범 <32606420+asdfghjkkl11@users.noreply.github.com>
2018-04-12 23:36:35 +0900

    Update HW3.c

--- HW3.c
+++ HW3.c
@@ -1,70 +1,101 @@
 #include <glut.h>
 #include <math.h>
 #define PI 3.141592
-GLfloat scale = 0.1;
-GLfloat px = 0.0;
-GLfloat py = 0.0;
-GLfloat pz = 0.0;
-GLint axis = 1;
-GLfloat R = 10;
 int delay = 20;
-int type = 1;
-GLfloat p1[2];
-GLfloat theta[3] = { 0 };
+GLfloat p1[3] = { 0,0,0 };//x,y,z
+GLfloat p2[3] = { 0,0,1 };//x,y,z
+GLfloat theta[2] = { 0,0 };
+GLfloat dx[2], dy[2], dz[2];
+GLfloat axis = 1;
+GLfloat p3[2];
 void init()
 {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glFrustum(-10.0, 10.0, -10.0, 10.0, -10.0, 10.0);
+	glFrustum(-1, 1, -1, 1, 1, 20);
 	glEnable(GL_DEPTH_TEST);
 	glMatrixMode(GL_MODELVIEW);
 }
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
+
 	glLoadIdentity(); // load an identity matrix
-	glColor3f(0, 0, 0);
-	glTranslatef(px, py, pz);
-	glRotatef(theta[0], 0.0, 1.0, 0.0);
-	glRotatef(-theta[1], 1.0, 0.0, 0.0);
-	glRotatef(theta[2], 0.0, 0.0, 1.0);
-	R = sqrt(pow(px, 2) + pow(py, 2) + pow(pz + 10, 2));
-	glutWireTeapot(scale);
+
+	gluLookAt(p1[0], p1[1], p1[2], p2[0], p2[1], axis* p2[2], 0, 1, 0);
+	glPushMatrix();
+	glTranslatef(0, 0, 2);
+	glColor3f(0.0, 0.8, 0.7);
+	glutWireTeapot(0.5);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(0, 0, -2);
+	glColor3f(1.0, 0.0, 0.0);
+	glutWireTeapot(0.5);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(2, 0, 0);
+	glColor3f(0.0, 1.0, 0.0);
+	glutWireTeapot(0.5);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(-2, 0, 0);
+	glColor3f(0.0, 0.0, 1.0);
+	glutWireTeapot(0.5);
+	glPopMatrix();
 	glFlush();
 }
 void keyboard_handler(unsigned char key, int x, int y)
 {
 	if (key == 'w') {
-		pz += 0.005;
-		scale += 0.005;
+		p1[0] += 0.01*sin((theta[0] * PI) / 180);
+		p1[2] += 0.01*cos((theta[0] * PI) / 180);
 	}
 	if (key == 'a') {
-		px += 0.01;
+		p1[0] += 0.01*sin(((theta[0] + 90) * PI) / 180);
+		p1[2] += 0.01*cos(((theta[0] + 90)  * PI) / 180);
+		p2[0] += 0.01*sin(((theta[0] + 90)  * PI) / 180);
+		p2[2] += 0.01*cos(((theta[0] + 90)  * PI) / 180);
 	}
 	if (key == 's') {
-		pz -= 0.005;
-		scale -= 0.005;
+		p1[0] -= 0.01*sin((theta[0] * PI) / 180);
+		p1[2] -= 0.01*cos((theta[0] * PI) / 180);
 	}
 	if (key == 'd') {
-		px -= 0.01;
+		p1[0] -= 0.01*sin(((theta[0] + 90)  * PI) / 180);
+		p1[2] -= 0.01*cos(((theta[0] + 90)  * PI) / 180);
+		p2[0] -= 0.01*sin(((theta[0] + 90)  * PI) / 180);
+		p2[2] -= 0.01*cos(((theta[0] + 90)  * PI) / 180);
 	}
+
 }
 void mouse_handler(int btn, int state, int x, int y)
 
 {
 	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		p1[0] = x;
-		p1[1] = y;
+		dx[0] = x;
+		dy[0] = y;
+
 	}
 	if (btn == GLUT_LEFT_BUTTON && state == GLUT_UP) {
-		px = px + ((p1[0] - x) / 25);
-		py = py - ((p1[1] - y) / 25);
-		pz = sqrt(pow(R, 2) - pow(px, 2) - pow(py, 2)) - 10;
-		theta[0] = ((atan2f(R, px) * 180 / PI) - 90) * 10;
-		theta[1] = ((atan2f(R, py) * 180 / PI) - 90) * 10;
-		theta[2] = ((atan2f(R, pz) * 180 / PI) - 90) * 10;
+		dx[1] = x;
+		dy[1] = y;
+		p3[0] = (dx[1] - dx[0]) / 500;
+		p3[1] = (dy[1] - dy[0]) / 500;
+		theta[0] += p3[0] * 30;
+		theta[1] += p3[1] * 30;
+		if (theta[0]>360)
+			theta[0] -= 360;
+		if (theta[1]>360)
+			theta[1] -= 360;
+		if (theta[0]<-360)
+			theta[0] += 360;
+		if (theta[1]<-360)
+			theta[1] += 360;
+		p2[0] = p1[0] + sin((theta[0] * PI) / 180);
+		p2[1] = p1[1] + sin((theta[1] * PI) / 180);
+		p2[2] = p1[2] + cos(((360 - theta[0]) * PI) / 180);
 	}
 
 }
